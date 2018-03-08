5b55b385fabcf213c187eebe4dad873bed5063f3
Hauke Brandes <hauke.brandes@parsqube.de>
2018-03-08 21:14:06 +0100

    Allow shell script to be symlinked

--- bin/fox.sh
+++ bin/fox.sh
@@ -10,7 +10,13 @@
 MODE="eval"
 SEP=/
 
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE="$0"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	SOURCE="$(readlink "$SOURCE")"
+	[[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_PATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 while getopts "?pbc" opt; do
     case "$opt" in
